e215add8d63b356938ec1d9d4234feb895a96546
Jeremy Ting <jeremyting02@gmail.com>
2024-11-25 11:16:13 +1300

    feat: styling in progress, moving elements around

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,37 @@
 import React, { useState } from "react";
 import ProductDetails from "./components/ProductDetails";
 import Header from "./components/Header";
+import styles from "./App.module.css";
 
 const App = () => {
-  const [cart, setCart] = useState([]);
+	const [cart, setCart] = useState([]);
 
-  const handleAddToCart = (product) => {
-    setCart((prevCart) => {
-      const existingItem = prevCart.find(
-        (item) => item.id === product.id && item.size === product.size
-      );
+	const handleAddToCart = (product) => {
+		setCart((prevCart) => {
+			const existingItem = prevCart.find((item) => item.id === product.id && item.size === product.size);
 
-      if (existingItem) {
-        return prevCart.map((item) =>
-          item === existingItem
-            ? { ...item, quantity: item.quantity + 1 }
-            : item
-        );
-      }
+			if (existingItem) {
+				return prevCart.map((item) => (item === existingItem ? { ...item, quantity: item.quantity + 1 } : item));
+			}
 
-      return [...prevCart, product];
-    });
-  };
+			return [...prevCart, product];
+		});
+	};
 
-  const handleUpdateQuantity = (product, delta) => {
-    setCart((prevCart) =>
-      prevCart
-        .map((item) =>
-          item === product ? { ...item, quantity: item.quantity + delta } : item
-        )
-        .filter((item) => item.quantity > 0)
-    );
-  };
+	const handleUpdateQuantity = (product, delta) => {
+		setCart((prevCart) =>
+			prevCart.map((item) => (item === product ? { ...item, quantity: item.quantity + delta } : item)).filter((item) => item.quantity > 0)
+		);
+	};
 
-  return (
-    <div>
-      <Header cart={cart} onUpdateQuantity={handleUpdateQuantity} />
-      <ProductDetails onAddToCart={handleAddToCart} />
-    </div>
-  );
+	return (
+		<div className={styles.appRoot}>
+			<div className={styles.appRoot2}>
+				<Header cart={cart} onUpdateQuantity={handleUpdateQuantity} />
+				<ProductDetails onAddToCart={handleAddToCart} />
+			</div>
+		</div>
+	);
 };
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import MiniCart from "./MiniCart";
+import styles from "./Header.module.css";
 
 const Header = ({ cart, onUpdateQuantity }) => {
-
-  return (
-    <div>
-      <MiniCart cart={cart} onUpdateQuantity={onUpdateQuantity} />
-    </div>
-  );
+	return (
+		<div className={styles.header}>
+			<MiniCart cart={cart} onUpdateQuantity={onUpdateQuantity} />
+		</div>
+	);
 };
 
 export default Header;
